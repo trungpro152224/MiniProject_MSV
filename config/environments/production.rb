45f6bb8072adc90a5a46baace949efeea7e91ca9
TRUNG <trungpro151224@gmail.com>
2018-03-28 12:14:45 +0700

    config heroku v3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,11 +26,11 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-  config.assets.compress = true
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
-  
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
